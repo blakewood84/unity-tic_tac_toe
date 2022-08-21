25234508bee10e140e2dcddfecdc62868df5d013
Blake Wood Jr <blakewoodjr84@gmail.com>
2022-08-20 20:07:09 -0700

    code cleanup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
         {0,3,6},
         {1,4,7},
         {2,5,8},
-        // Diagnal Rows
+        // Diagonal Rows
         {0,4,8},
         {2,4,6},
     };
